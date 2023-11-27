b235ee025ebdeb8d92bf3e5bb0cd547307bc4ceb
Stanislaav Kozh <printks@yandex.ru>
2023-11-27 21:13:39 +0300

    injection string and other value

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -2,6 +2,8 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
 //    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
@@ -20,5 +22,21 @@ public class Person {
     public void callYourPet() {
         System.out.println("Hello my lovely Pet!");
         pet.say();
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
