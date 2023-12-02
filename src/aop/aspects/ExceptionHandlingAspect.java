6b147f798654c8055982186994045fd8909d0efb
Stanislaav Kozh <printks@yandex.ru>
2023-12-02 21:21:43 +0300

    the order of execution pointcut

---
+++ src/aop/aspects/ExceptionHandlingAspect.java
@@ -0,0 +1,18 @@
+package aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(30)
+public class ExceptionHandlingAspect {
+
+    @Before("aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetExceptionHandlingAspect(){
+        System.out.println("beforeGetExceptionHandlingAspect: ловим/обрабатываем исключение " +
+                "при потытки получить книгу/журнал");
+    }
+}
--- src/aop/aspects/LoggingAspect.java
+++ src/aop/aspects/LoggingAspect.java
@@ -3,47 +3,42 @@ package aop.aspects;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Order(10)
 public class LoggingAspect {
 
-    @Pointcut("execution(* aop.UniLibrary.get*())")
-    private void allGetMethodFromUniLibrary(){}
-
-    @Pointcut("execution(* aop.UniLibrary.return*())")
-    private void allReturnMethodFromUniLibrary(){}
-
-    @Pointcut("allGetMethodFromUniLibrary() || allReturnMethodFromUniLibrary()")
-    private void allGetAndReturnMethodFromUniLibrary(){}
-
-    @Before("allGetMethodFromUniLibrary()")
-    public void beforeGetLoggingAdvice(){
-        System.out.println("beforeGetLoggingAdvice: writing Log#1");
-    }
-
-    @Before("allReturnMethodFromUniLibrary()")
-    public void beforeReturnLoggingAdvice(){
-        System.out.println("beforeReturnLoggingAdvice: writing Log#2");
-    }
-
-    @Before("allGetAndReturnMethodFromUniLibrary()")
-    public void beforeGetAndReturnLoggingAdvice(){
-        System.out.println("allGetAndReturnMethodFromUniLibrary: writing Log#3");
-    }
-
-
-//    @Pointcut("execution(* get*())")
-//    private void allGetMethods(){}
+//    @Pointcut("execution(* aop.UniLibrary.get*())")
+//    private void allGetMethodFromUniLibrary(){}
 //
-//    @Before("allGetMethods()")
+//    @Pointcut("execution(* aop.UniLibrary.return*())")
+//    private void allReturnMethodFromUniLibrary(){}
+//
+//    @Pointcut("allGetMethodFromUniLibrary() || allReturnMethodFromUniLibrary()")
+//    private void allGetAndReturnMethodFromUniLibrary(){}
+//
+//    @Before("allGetMethodFromUniLibrary()")
 //    public void beforeGetLoggingAdvice(){
-//        System.out.println("beforeGetLoggingAdvice: попытка получить книгу");
+//        System.out.println("beforeGetLoggingAdvice: writing Log#1");
 //    }
 //
-//    @Before("allGetMethods()")
-//    public void beforeGetSecurityAdvice(){
-//        System.out.println("beforeGetSecurityAdvice: првоерка прав на получение книгу");
+//    @Before("allReturnMethodFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println("beforeReturnLoggingAdvice: writing Log#2");
 //    }
+//
+//    @Before("allGetAndReturnMethodFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice(){
+//        System.out.println("allGetAndReturnMethodFromUniLibrary: writing Log#3");
+//    }
+
+    @Before("aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetLoggingAdvice(){
+        System.out.println("beforeGetLoggingAdvice: логирование попытки получить книгу");
+    }
+
 }
+
