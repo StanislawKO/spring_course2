0e2d9e18e3fa3ec5c214a90057b919337fc5664e
Stanislaav Kozh <printks@yandex.ru>
2023-12-02 19:39:45 +0300

    add pointcut without parameters

---
+++ src/aop/SchoolLibrary.java
@@ -0,0 +1,16 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchoolLibrary extends AbstractLibrary {
+
+    @Override
+    public void getBook() {
+        System.out.println("Мы берем книгу из SchoolLibrary");
+    }
+
+    public void getMagazine() {
+        System.out.println("Мы берем журнал из SchoolLibrary");
+    }
+}
